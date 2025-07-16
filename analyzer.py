1aded41f44571ce9fef91d03742c134c792646f2
sahlsaad4 <myacc.pc2002@gmail.com>
2025-07-16 15:28:13 +0300

    Create analyzer.py
    # تمرين على استخدام while loop لتحليل الأرقام والكلمات
    # يجمع إحصائيات عن الأعداد (المجموع، الأصغر، الأكبر، الزوجي والفردي)
    # ويحسب عدد الكلمات والحروف ويعرض تقريرًا نهائيًا

---
+++ analyzer.py
@@ -0,0 +1,53 @@
+total = 0
+numbers = []
+the_2 = 0
+the_1 = 0
+numb_of_numbers = 0
+words = []
+countted_letters = []
+countted_letters_grob_2 = []
+min_number = 0
+max_numb = 0
+
+while True:
+    message_box = input("(write 'END' to stop)\nwrite massage):")
+    if message_box == "END":
+        break
+
+    if message_box.isdigit():
+        numb_of_numbers += 1
+        if numb_of_numbers == 1:
+            max_numb = int(message_box)
+            min_number = int(message_box)
+        else:
+            if int(message_box) > max_numb:
+                max_numb = int(message_box)
+            if int(message_box) < min_number:
+                min_number = int(message_box)
+        total += int(message_box)
+        numbers.append(int(message_box))  # ← تم تصحيح هذا السطر
+
+        if int(message_box) % 2 == 0:
+            the_2 += 1
+        if int(message_box) % 2 != 0:
+            the_1 += 1
+
+    if any(ch.isalpha() for ch in message_box):
+        words += message_box.split()
+        for ch in message_box:
+            countted_letters.append(ch)
+
+for n in countted_letters:
+    if n not in countted_letters_grob_2:
+        countted_letters_grob_2 += n
+        count = countted_letters.count(n)
+        print(f"{n}:{count}")
+
+print(f"مجموه الارقام ={total}")
+print(f"عدد الارقام ={numb_of_numbers}")
+print(f"عدد الارقام الزوجية ={the_2}")
+print(f"عدد الارقام الفردية ={the_1}")
+print(f"اصغر رقم ={min_number}")
+print(f"اكبر رقم ={max_numb}")
+print(f"عدد الكلمات ={len(words)}")
+print(f"عدد الحروف={len(countted_letters)}")
